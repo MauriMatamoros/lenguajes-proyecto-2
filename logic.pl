d0e4e4e9be79f49cbcfce4b108fe7b2d8a472968
StrScr <andres.mejia@unitec.edu>
2017-09-20 10:02:01 -0600

    Changed rule names to reflect purpose and avoid collisions.

--- logic.pl
+++ logic.pl
@@ -14,27 +14,26 @@ route(From, To, [From | Rest], Visited) :-
     not(inlist(To, Visited)), not(inlist(From, Visited)),
     hasflight(From, Next), route(Next, To, Rest, [From | Visited]).
 
-/*Route, Stay in Particular Airline*
-route(From, To, Airline, Travel) :- route(From, To, Airline, Travel, []).
-route(From, To, Airline, [From | [To]], _) :- hasflight(From, To, Airline).
-route(From, To, Airline, [From | Rest], Visited) :-
+/*Route with Airline*/
+aroute(From, To, Airline, Travel) :- aroute(From, To, Airline, Travel, []).
+aroute(From, To, Airline, [From | [To]], _) :- hasflight(From, To, Airline).
+aroute(From, To, Airline, [From | Rest], Visited) :-
     len(Visited, X), X<3,
     not(inlist(To, Visited)), not(inlist(From, Visited)),
-    hasflight(From, Next, Airline), route(Next, To, Airline, Rest, [From | Visited]).
-*/
+    hasflight(From, Next, Airline), aroute(Next, To, Airline, Rest, [From | Visited]).
 
-/*Route, Cheapest*/
-route(From, To, Airline, Travel, Cost) :- route(From, To, Airline, Travel, [], Cost).
-route(From, To, Airline, [From | [To]], _, Cost) :- hasflight(From, To, Airline, Cost).
-route(From, To, Airline, [From | Rest], Visited, Cost) :-
+/*Route with Airline & Cost*/
+croute(From, To, Airline, Travel, Cost) :- croute(From, To, Airline, Travel, [], Cost).
+croute(From, To, Airline, [From | [To]], _, Cost) :- hasflight(From, To, Airline, Cost).
+croute(From, To, Airline, [From | Rest], Visited, Cost) :-
     len(Visited, X), X<3,
     not(inlist(To, Visited)), not(inlist(From, Visited)),
-    hasflight(From, Next, Airline, CX), route(Next, To, Airline, Rest, [From | Visited], CY),
+    hasflight(From, Next, Airline, CX), croute(Next, To, Airline, Rest, [From | Visited], CY),
     Cost is CX+CY.
 
 listroutes(From, To, Airline, Routes) :- setof(C-R, route(From, To, Airline, R, C), Routes).
 
-cheapestroute(From, To, Airline, Route, Cost) :- setof(C-Airline-R, route(From, To, Airline, R, C), [Cost-Airline-Route|_]).
+cheapestroute(From, To, Airline, Route, Cost) :- setof(C-Airline-R, croute(From, To, Airline, R, C), [Cost-Airline-Route|_]).
 
 /*shortestroute(From, To, Flight).*/
 
